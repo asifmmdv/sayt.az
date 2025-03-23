86b408de50b3e612fb151ae6f339db3147043723
asifmmdv <mamedov_asif@icloud.com>
2025-03-23 16:35:29 +0400

    fix0032

--- js/header.js
+++ js/header.js
@@ -24,18 +24,13 @@ function opendd(x) {
 function opensb() {
     const sidebar = document.getElementById('sidebar');
     const alldropdowns = document.querySelectorAll('.switch');
-    if (window.innerWidth < 1280) { // Check if the window size is greater than 1280px
 
-        // Close all dropdowns if any are open
-        alldropdowns.forEach(dropdown => {
-            dropdown.classList.remove('switch');
-        });
+    // Close all dropdowns if any are open
+    alldropdowns.forEach(dropdown => {
+        dropdown.classList.remove('switch');
+    });
 
-        // Toggle the sidebar
-        sidebar.classList.toggle('opensb');
-    }
-    else {
-        sidebar.classList.remove('opensb');
-    }
+    // Toggle the sidebar
+    sidebar.classList.toggle('opensb');
 }
 
